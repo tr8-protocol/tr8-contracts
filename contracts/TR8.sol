1e5449134492bc6e86f28bfc02a2bbd608a6863b
markcarey <mark@markcarey.com>
2023-08-11 11:55:34 -0400

    stack too deep - argh

--- contracts/TR8.sol
+++ contracts/TR8.sol
@@ -3,25 +3,34 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol";
-import { ERC2771Context } from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-import { SchemaResolver } from "@ethereum-attestation-service/eas-contracts/contracts/resolver/SchemaResolver.sol";
+import { ERC2771ContextUpgradeable } from "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
+import { SchemaResolver } from "./utils/SchemaResolver.sol";
 import { IEAS, Attestation } from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
+import "@layerzerolabs/solidity-examples/contracts/contracts-upgradable/lzApp/NonblockingLzAppUpgradeable.sol";
 
 // TODO: finalize interface
 interface ITR8Nft {
-    function initialize(string calldata _name, string calldata _symbol, address _admin, address _owner) external;
+    //function initialize(string calldata _name, string calldata _symbol, address _admin, address _owner, address hook, address[] calldata issuers, address[] calldata claimers, bool allowTransfers) external;
+    function initialize(Attestation calldata attestation) external;
     function mint(address to, uint256 value) external;
     function safeMint(address to, uint256 tokenId) external;
     function exists(uint256 tokenId) external view returns (bool);
     function tokenURI(uint256 tokenId) external view returns (string memory);
     function hasRole(bytes32 role, address account) external view returns (bool);
+    function burn(uint256 tokenId) external;
+}
+
+interface ITR8Hook {
+    function onMint(Attestation calldata attestation, uint256 value, address nftAddress) external returns (bool);
+    function onBurn(Attestation calldata attestation, uint256 value, address nftAddress) external returns (bool);
 }
 
 /**
  * @title EAS Resolver, NFT Factory, and Transporter for TR8 Protocol
  */
-contract TR8 is SchemaResolver, ERC2771Context {
+contract TR8 is Initializable, SchemaResolver, NonblockingLzAppUpgradeable, ERC2771ContextUpgradeable {
     //using Address for address;
 
     bool public homeChain;
@@ -41,8 +50,26 @@ contract TR8 is SchemaResolver, ERC2771Context {
     error ExpiredDrop();
     error InvalidNameSpace();
 
-    constructor(IEAS eas) SchemaResolver(eas) ERC2771Context(0xb539068872230f20456CF38EC52EF2f91AF4AE49) {}
+    //constructor(IEAS eas, address _lzEndpoint)
+    //    SchemaResolver(eas) 
+    //    ERC2771Context(0xb539068872230f20456CF38EC52EF2f91AF4AE49) 
+    //    NonblockingLzApp(_lzEndpoint) 
+    //{}
 
+    constructor() ERC2771ContextUpgradeable(0xb539068872230f20456CF38EC52EF2f91AF4AE49) {
+        //_disableInitializers();
+    }
+
+    function initialize(IEAS eas, address _lzEndpoint, address _nftImplementation) initializer public {
+        __SchemaResolver_init(eas);
+        __NonblockingLzAppUpgradeable_init(_lzEndpoint);
+        nftImplementation = _nftImplementation;
+    }
+
+    struct Attribute {
+        string key;
+        string value;
+    }
 
     event TR8DropCreated(
         address indexed owner,
@@ -52,48 +79,66 @@ contract TR8 is SchemaResolver, ERC2771Context {
     // EAS Schema Resolver:
 
     function onAttest(Attestation calldata attestation, uint256 value) internal override returns (bool) {
-        // TODO: check schema? 
-        // TODO: check for drop creation schema first?
         if (attestation.schema == dropSchema) {
             // This is a drop creation attestation
-            (string memory _name, string memory _symbol, string memory _nameSpace) = abi.decode(attestation.data, (string, string, string));
-            nftForDrop[attestation.uid] = _cloneNFT(attestation.schema, _name, _symbol, attestation.attester);
-            // check nameSpace is owned by attester
-            if (_nameSpaceExists(_nameSpace)) {
-                if (!_ownsNameSpace(attestation.attester, _nameSpace)) {
-                    revert InvalidNameSpace();
-                }
-            } else {
-                nftsForNameSpace[keccak256(abi.encodePacked(_nameSpace))].push(nftForDrop[attestation.uid]);
-            }
+            return _newDrop(attestation);
         } else {
             // This is a minting attestation
-            if (nftForDrop[attestation.refUID] == address(0)) {
-                revert InvalidDrop();
-            }
-            Attestation memory drop = _eas.getAttestation(attestation.refUID);
-            if (drop.expirationTime < block.timestamp) {
-                // minting period has ended
-                revert ExpiredDrop();
-            }
-            // TODO: call hook
-
-            // TODO: who gets NFT? attester or receipient? or always recipeint regardless of who attested?
-            ITR8Nft(nftForDrop[attestation.refUID]).safeMint(attestation.recipient, uint256(attestation.uid));
+            return _newMint(attestation, value);
         }
-
-        return true;
     }
 
     function onRevoke(Attestation calldata attestation, uint256 value) internal override returns (bool) {
-        // TODO: check schema?
+        Attestation memory drop = _eas.getAttestation(attestation.refUID);
+        (/*string memory _nameSpace*/, /*string memory _name*/, /*string memory _symbol*/, /*string memory description*/, /*string memory image*/, address hook, /*address[] memory claimers*/, /*address[] memory issuers*/, /*string memory secret*/, /*Attribute[] memory attributes*/, /*string[] memory tags*/, /*bool allowTransfers*/) = abi.decode(drop.data, (string, string, string, string, string, address, address[], address[], string, Attribute[], string[], bool));
+        // call hook
+        if (hook != address(0)) {
+            ITR8Hook(hook).onBurn(attestation, value, nftForDrop[attestation.refUID]);
+        }
+        if ( ITR8Nft(nftForDrop[attestation.refUID]).exists(uint256(attestation.uid)) ) {
+            ITR8Nft(nftForDrop[attestation.refUID]).burn(uint256(attestation.uid));
+        }
+        return true;
+    }
 
-        // TODO: call hook
-
+    function _newMint(Attestation calldata attestation, uint256 value) internal returns (bool) {
+        if (nftForDrop[attestation.refUID] == address(0)) {
+            revert InvalidDrop();
+        }
+        Attestation memory drop = _eas.getAttestation(attestation.refUID);
+        if (drop.expirationTime < block.timestamp) {
+            // minting period has ended
+            revert ExpiredDrop();
+        }
+        (/*string memory _nameSpace*/, /*string memory _name*/, /*string memory _symbol*/, /*string memory description*/, /*string memory image*/, address hook, /*address[] memory claimers*/, /*address[] memory issuers*/, /*string memory secret*/, /*Attribute[] memory attributes*/, /*string[] memory tags*/, /*bool allowTransfers*/) = abi.decode(drop.data, (string, string, string, string, string, address, address[], address[], string, Attribute[], string[], bool));
+        // call hook
+        if (hook != address(0)) {
+            // hook can revert or add MINTER_ROLE to recipient or ISSUER_ROLE to attester ... and do other stuff
+            ITR8Hook(hook).onMint(attestation, value, nftForDrop[attestation.refUID]);
+        }
+        if ( ITR8Nft(nftForDrop[attestation.refUID]).hasRole(ISSUER_ROLE, attestation.attester) ||
+            ITR8Nft(nftForDrop[attestation.refUID]).hasRole(MINTER_ROLE, attestation.recipient) ) {
+            // recipient gets NFT
+            ITR8Nft(nftForDrop[attestation.refUID]).safeMint(attestation.recipient, uint256(attestation.uid));
+        }
         return true;
     }
 
     // TR8 Factory
+
+    function _newDrop(Attestation calldata attestation) internal returns (bool) {
+        (string memory _nameSpace, /*string memory _name*/, /*string memory _symbol*/, /*string memory description*/, /*string memory image*/, /*address hook*/, /*address[] memory claimers*/, /*address[] memory issuers*/, /*string memory secret*/, /*Attribute[] memory attributes*/, /*string[] memory tags*/, /*bool allowTransfers*/) = abi.decode(attestation.data, (string, string, string, string, string, address, address[], address[], string, Attribute[], string[], bool));
+        //nftForDrop[attestation.uid] = _cloneNFT(attestation.uid, _name, _symbol, attestation.attester, hook, issuers, claimers, allowTransfers);
+        nftForDrop[attestation.uid] = _cloneNFT(attestation);
+        if (_nameSpaceExists(_nameSpace)) {
+            if (!_ownsNameSpace(attestation.attester, _nameSpace)) {
+                revert InvalidNameSpace();
+            }
+        } else {
+            nftsForNameSpace[keccak256(abi.encodePacked(_nameSpace))].push(nftForDrop[attestation.uid]);
+        }
+        return true;
+    }
 
     // public functions
     function nameSpaceExists(string calldata _nameSpace) external view returns (bool) {
@@ -115,9 +160,11 @@ contract TR8 is SchemaResolver, ERC2771Context {
     }
 
     // @dev deploys a TR8Nft contract
-    function _cloneNFT(bytes32 salt, string memory _name, string memory _symbol, address owner) internal returns (address) {
-        address clone = Clones.cloneDeterministic(nftImplementation, salt);
-        ITR8Nft(clone).initialize(_name, _symbol, _msgSender(), owner);
+    // xfunction _cloneNFT(bytes32 salt, string memory _name, string memory _symbol, address owner, address hook, address[] memory issuers, address[] memory claimers, bool allowTransfers) internal returns (address) {
+    function _cloneNFT(Attestation calldata attestation) internal returns (address) {
+        address clone = Clones.cloneDeterministic(nftImplementation, attestation.uid);
+        //ITR8Nft(clone).initialize(_name, _symbol, _msgSender(), owner, hook, issuers, claimers, allowTransfers);
+        ITR8Nft(clone).initialize(attestation);
         emit TR8DropCreated(_msgSender(), clone);
         return clone;
     }
@@ -133,6 +180,19 @@ contract TR8 is SchemaResolver, ERC2771Context {
 
     function _nameSpaceExists(string memory _nameSpace) internal view returns (bool) {
         return nftsForNameSpace[keccak256(abi.encodePacked(_nameSpace))].length > 0;
+    }
+
+    // LayerZero Transporter
+    function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory) internal override {
+        // TODO
+    }
+
+    function _msgSender() internal view override(ERC2771ContextUpgradeable, ContextUpgradeable) returns (address) {
+        return super._msgSender();
+    }
+
+    function _msgData() internal view override(ERC2771ContextUpgradeable, ContextUpgradeable) returns (bytes calldata) {
+        return super._msgData();
     }
 
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 800,
+      details: {
+        yul: false,
+      },
+    },
+    viaIR : true,
+  }
 };
